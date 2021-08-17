98b97b80d7d8da6981b9234241671d22bf21f65a
Ali.G <Alipishbiinn7878@gmail.com>
2021-08-17 23:20:23 +0430

    responsive added

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnBurger = document.querySelector(".burger");
+const btnClose = document.querySelector(".close");
+const sidebar = document.querySelector(".sidebar");
+
+btnBurger.addEventListener("click", function (e) {
+  e.preventDefault();
+  sidebar.style.left = 0;
+});
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  sidebar.style.left='-100%'
+});
